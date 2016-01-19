787ff8bf79e2edb171252727c9ff3996df2e513f
Jeremy Weinstein <jeremydw@gmail.com>
2016-01-19 11:52:35 -0800

    Initial commit.

--- process2.py
+++ process2.py
@@ -10,27 +10,37 @@ KEY = 'AIzaSyCoY05WH-mI8y3_NDUcwEzvjznLR1YAInQ'
 URL = 'https://maps.googleapis.com/maps/api/geocode/json?key={}&address='.format(KEY)
 NSMAP = {'kml': 'http://www.opengis.net/kml/2.2'}
 COORDINATES = 'kml:Polygon/kml:outerBoundaryIs/kml:LinearRing/kml:coordinates'
+COORDINATES_2 = 'kml:MultiGeometry/kml:Polygon/kml:outerBoundaryIs/kml:LinearRing/kml:coordinates'
 
 
-def get_neighborhood_from_kml(results, kml_path):
-  test_lat, test_lon = (results['geometry']['location']['lat'],
-                        results['geometry']['location']['lng'])
+MARKETS_TO_KMLS = {
+    'Atlanta': 'atlanta.kml',
+    'Charlotte': 'charlotte.kml',
+    'Nashville': 'nashville.kml',
+    'Raleigh-Durham': 'rdu.kml',
+    'Salt Lake City': 'slc.kml',
+}
+
+
+def get_neighborhood_from_kml(lat_lon, kml_path):
+  test_lat, test_lon = lat_lon
   test_point = Point(float(test_lat), float(test_lon))
   kml = etree.parse(kml_path)
   placemarks = kml.findall('//kml:Placemark', namespaces=NSMAP)
   found_neighborhood = None
-  for placemark in placemarks:
-    el = placemark.find(COORDINATES, namespaces=NSMAP)
+  for el in kml.findall('.//kml:coordinates', namespaces=NSMAP):
     coords = []
     for coord in el.text.split(' '):
       lat, lon, _ = coord.split(',')
       coords.append((float(lon), float(lat)))
     poly = MultiPoint(coords).convex_hull
     if poly.contains(test_point):
-      name = placemark.find('kml:name', namespaces=NSMAP)
-      found_neighborhood = name.text
-  if found_neighborhood is None:
-    return get_neighborhood(results)
+      placemark = el.getparent().getparent().getparent().getparent()
+      if 'MultiGeometry' in str(placemark.tag):
+        placemark = placemark.getparent()
+      val = placemark.find('kml:ExtendedData/kml:Data[@name=\'32CitiesNa\']/kml:value', namespaces=NSMAP)
+      if val is not None:
+        found_neighborhood = val.text
   return found_neighborhood
 
 
@@ -54,32 +64,19 @@ def process(path, kml_path=None):
     is_json = True
     rows = json.load(open(path))
   for row in rows:
-    if is_csv:
-      address = '{}, {}, {} {}'.format(
-          row['Primary Street Address'],
-          row['Primary City'],
-          row['Primary State'],
-          row['Primary Zip Code'])
-    if is_json:
-      address = row.get('Address', row.get(' Address'))
-    url = URL + urllib.quote(address)
-    resp = urllib.urlopen(url).read()
-    data = json.loads(resp)
-    results = data['results']
-    if not results:
-      raise Exception('{}; UNABLE TO GEOCODE'.format(address))
-    results = data['results'][0]
-    if kml_path:
-      neighborhood = get_neighborhood_from_kml(results, kml_path)
-      neighborhood = neighborhood.encode('utf-8')
-    else:
-      neighborhood = get_neighborhood(results)
-    print '{}; {}'.format(address, neighborhood)
+    lat_lon = (row['Latitude'], row['Longitude'])
+    kml_path = MARKETS_TO_KMLS[row['Market']]
+    if 'rdu' not in kml_path:
+#    if 'atlanta' not in kml_path and 'rdu' not in kml_path:
+      continue
+    neighborhood = get_neighborhood_from_kml(lat_lon, kml_path)
+    address = row['Property Address']
+    print '{} -> {}'.format(address, neighborhood)
 
 
 
 def main():
-  process('austin.json', 'austin.kml')
+  process('idol.csv')
 
 
 if __name__ == '__main__':
